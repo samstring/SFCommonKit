f3ba15a42c539ae1e04606fb3633aeb8b386b258
samstring <1264986115@qq.com>
2020-05-13 19:14:27 +0800

    初始化

---
+++ Example/SFCommonKit/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  SFCommonKit
+//
+//  Created by samstring on 05/13/2020.
+//  Copyright (c) 2020 samstring. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/SFCommonKit/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  SFCommonKit
+//
+//  Created by samstring on 05/13/2020.
+//  Copyright (c) 2020 samstring. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SFViewController : UIViewController
+
+@end
